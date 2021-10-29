77ed1b86e15991ef7c3841d99d24817cf383d23a
Paolo Vargas <paolo9517@gmail.com>
2021-10-29 13:13:22 -0300

    Table Migrations Done

--- db/migrate/20211029133848_remove_columns_from_list.rb
+++ db/migrate/20211029133848_remove_columns_from_list.rb
@@ -3,6 +3,6 @@ class RemoveColumnsFromList < ActiveRecord::Migration[6.0]
     remove_column :lists, :location
     remove_column :lists, :price_rate
     remove_column :lists, :listing_title
-    remove_column :lists, :user_id
+    remove_references :lists, :user, index: true, foreign_key: true
   end
 end
