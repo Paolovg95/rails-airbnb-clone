77d49cbb3d227f4e02306d6f2a4dca748d91f6c1
Diego Guerra <diegoguerradiaz@gmail.com>
2021-10-30 16:16:25 -0300

    created code for markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -10,7 +10,17 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
+    if (mapElement) {
+      // [ ... ]
+      const markers = JSON.parse(mapElement.dataset.markers);
+      markers.forEach((marker) => {
+        new mapboxgl.Marker()
+          .setLngLat([marker.lng, marker.lat])
+          .addTo(map);
+      });
+    }
   }
 };
+
 
 export { initMapbox };
