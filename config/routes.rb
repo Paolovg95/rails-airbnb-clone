eaae1b27ed8501f03ea9324a1abc173c770696dd
Paolo Vargas <paolo9517@gmail.com>
2021-11-05 16:54:15 -0300

    AOS Gem added, routes and Booking Views

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,9 @@ Rails.application.routes.draw do
   end
 
   resources :bookings, only: [:index]
+  # namespace :account do
+  #   resources :offers
+  #   resources :bookings
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
